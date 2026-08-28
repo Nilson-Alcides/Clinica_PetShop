ea926d4a694cb4e99fd4e50bc4dc736fdda1c782
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-08-28 15:21:44 -0300

    add projeto

---
+++ ClinicaVeterinaria_3DS/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using ClinicaVeterinaria_3DS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClinicaVeterinaria_3DS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ClinicaVeterinaria_3DS/Models/Tutor.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaVeterinaria_3DS.Models
+{
+    public class Tutor
+    {
+        /*
+        create table TOTOR(
+        id_tutor int primary key auto_increment,
+        nome varchar(50) not null,
+        telefone varchar(15) not null,
+        email varchar(30) not null
+        );         
+         */
+        [Display(Name = "Codigo")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O nome do tutor é obrigatorio")]
+        [Display(Name = "Nome")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "O Telefone do tutor é obrigatorio")]
+        [Display(Name = "Telefone")]
+        public string telefone { get; set; }
+
+        [Required(ErrorMessage = "O E-mail do tutor é obrigatorio")]
+        [Display(Name = "E-mail")]
+        public string email { get; set; }
+    }
+}
