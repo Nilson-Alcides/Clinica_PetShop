d4ec008fc26f7e5aa0041361c32de8945c681cb2
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-08-28 15:26:30 -0300

    update classe raça

--- ClinicaVeterinaria_3DS/Models/Raca.cs
+++ ClinicaVeterinaria_3DS/Models/Raca.cs
@@ -1,6 +1,20 @@
-﻿namespace ClinicaVeterinaria_3DS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaVeterinaria_3DS.Models
 {
     public class Raca
     {
+        /*
+        create table RACA(
+        id_raca int primary key auto_increment,
+        nome varchar(30) not null
+        );
+         */
+        [Display(Name = "Codigo")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O nome do tutor é obrigatorio")]
+        [Display(Name = "Nome")]
+        public string nome { get; set; }
     }
 }
